7aea2b7e840a8fc6572c3e44e7d6300c2c73b34c
ryancoakley <ryan.coakley93@gmail.com>
2014-03-05 13:48:45 -0500

    original

---
+++ skew.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include "arrayUtils.h"
+
+using namespace std;
+
+// Determine if a data set is skewed
+void skew ()
+{
+  /** Insert your code here **/
+}
+
+
+int main()
+{
+  skew();
+  return 0;
+}
+
