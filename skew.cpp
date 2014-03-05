3290fb3bcda7a6230a2424927dd422fb57d54bef
seffland <seffland@gmail.com>
2014-03-05 16:44:39 -0500

    Data structure built
    Reads last number two times.

--- skew.cpp
+++ skew.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include "arrayUtils.h"
 
 using namespace std;
 
 // Determine if a data set is skewed
-void skew ()
+void skew (float data[])
 {
-  /** Insert your code here **/
-}
 
+}
 
 int main()
 {
-  skew();
-  return 0;
-}
+    float data [10000];
+    int arraySize = 0;
+    ifstream myfile;
+    myfile.open ("test0.in");
+    float temp;
+    while(myfile)
+    {
+        myfile >> temp;
+        addToEnd(data, arraySize, temp);
+    }
 
+    for(int i = 0; i < arraySize; i++)
+    {
+        cout << data[i] << endl;
+    }
+    skew(data);
+    return 0;
+}
