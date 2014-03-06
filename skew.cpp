6a20375507058f91d965c3df829b14c8d804f4b6
seffland <seffland@gmail.com>
2014-03-06 14:32:06 -0500

    Finished product
    Checks for both test cases

--- skew.cpp
+++ skew.cpp
@@ -28,13 +28,33 @@ float calculateMean(float data[], int arraySize)
     return (sum/arraySize);
 }
 
+float calculateMedian(float data[], int arraySize)
+{
+    insertionSort(data, arraySize);
+    if ( arraySize % 2== 0 ) //ODD
+    {
+        double high, low;
+        low = data[arraySize/2];
+        high = data[(arraySize/2)+1];
+        return ((high+low)/2);
+    }
+    else //EVEN
+    {
+        return (data[arraySize/2]);
+    }
+}
+
 // Determine if a data set is skewed
-void skew (float data[], int arraySize)
+float skew (float data[], int arraySize)
 {
     float mean = calculateMean(data, arraySize);
     //cout << "Mean = " << mean;
 
     float deviation = calculateDeviation(data, arraySize, mean);
+    float median = calculateMedian(data, arraySize);
+    float numerator = 3*(mean - median);
+    float denomirator = deviation;
+    return (numerator/denomirator);
 }
 
 int main()
@@ -53,6 +73,15 @@ int main()
     {
         cout << data[i] << endl;
     }
-    skew(data, arraySize);
+    float skewValue = skew(data, arraySize);
+    //cout << "SKEW VALUE= " << skewValue << endl;
+    if (skewValue > 1 || skewValue <-1)
+    {
+        cout << "Badly skewed";
+    }
+    else
+    {
+        cout << "Not badly skewed";
+    }
     return 0;
 }
